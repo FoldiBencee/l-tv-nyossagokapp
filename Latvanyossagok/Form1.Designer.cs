4b92452b123a083748d879cb1cefe0f85186abde
Földi Bence <foldib555@gmail.com>
2019-12-12 22:35:06 +0100

    eddig jutottam

---
+++ Latvanyossagok/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Latvanyossagok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonkuldes = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lakossagnumeric = new System.Windows.Forms.NumericUpDown();
+            this.buttonkilepes = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonlatvanyossag = new System.Windows.Forms.Button();
+            this.textboxnev = new System.Windows.Forms.TextBox();
+            this.textBoxleiras = new System.Windows.Forms.TextBox();
+            this.labellatvanyossag = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelvaros = new System.Windows.Forms.Label();
+            this.comboBoxvaros = new System.Windows.Forms.ComboBox();
+            this.latvanyossagarnumeric = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listatorlesbutton = new System.Windows.Forms.Button();
+            this.listamodositbutton = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.lakossagnumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.latvanyossagarnumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonkuldes
+            // 
+            this.buttonkuldes.Location = new System.Drawing.Point(160, 32);
+            this.buttonkuldes.Name = "buttonkuldes";
+            this.buttonkuldes.Size = new System.Drawing.Size(75, 23);
+            this.buttonkuldes.TabIndex = 0;
+            this.buttonkuldes.Text = "kuldes";
+            this.buttonkuldes.UseVisualStyleBackColor = true;
+            this.buttonkuldes.Click += new System.EventHandler(this.buttonkuldes_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "adja meg a telepules nevet amit fel akar venni";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "adja meg a telepules letszamat";
+            // 
+            // lakossagnumeric
+            // 
+            this.lakossagnumeric.Location = new System.Drawing.Point(171, 72);
+            this.lakossagnumeric.Name = "lakossagnumeric";
+            this.lakossagnumeric.Size = new System.Drawing.Size(120, 20);
+            this.lakossagnumeric.TabIndex = 4;
+            // 
+            // buttonkilepes
+            // 
+            this.buttonkilepes.Location = new System.Drawing.Point(361, 291);
+            this.buttonkilepes.Name = "buttonkilepes";
+            this.buttonkilepes.Size = new System.Drawing.Size(75, 23);
+            this.buttonkilepes.TabIndex = 5;
+            this.buttonkilepes.Text = "kilepes";
+            this.buttonkilepes.UseVisualStyleBackColor = true;
+            this.buttonkilepes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "latvanyossagfelvetele:";
+            // 
+            // buttonlatvanyossag
+            // 
+            this.buttonlatvanyossag.Location = new System.Drawing.Point(139, 118);
+            this.buttonlatvanyossag.Name = "buttonlatvanyossag";
+            this.buttonlatvanyossag.Size = new System.Drawing.Size(131, 23);
+            this.buttonlatvanyossag.TabIndex = 7;
+            this.buttonlatvanyossag.Text = "latvanyossagfelvetel";
+            this.buttonlatvanyossag.UseVisualStyleBackColor = true;
+            this.buttonlatvanyossag.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textboxnev
+            // 
+            this.textboxnev.Location = new System.Drawing.Point(79, 164);
+            this.textboxnev.Name = "textboxnev";
+            this.textboxnev.Size = new System.Drawing.Size(100, 20);
+            this.textboxnev.TabIndex = 8;
+            // 
+            // textBoxleiras
+            // 
+            this.textBoxleiras.Location = new System.Drawing.Point(79, 190);
+            this.textBoxleiras.Name = "textBoxleiras";
+            this.textBoxleiras.Size = new System.Drawing.Size(100, 20);
+            this.textBoxleiras.TabIndex = 9;
+            // 
+            // labellatvanyossag
+            // 
+            this.labellatvanyossag.AutoSize = true;
+            this.labellatvanyossag.Location = new System.Drawing.Point(7, 167);
+            this.labellatvanyossag.Name = "labellatvanyossag";
+            this.labellatvanyossag.Size = new System.Drawing.Size(25, 13);
+            this.labellatvanyossag.TabIndex = 11;
+            this.labellatvanyossag.Text = "nev";
+            this.labellatvanyossag.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "leiras";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ar";
+            // 
+            // labelvaros
+            // 
+            this.labelvaros.AutoSize = true;
+            this.labelvaros.Location = new System.Drawing.Point(7, 240);
+            this.labelvaros.Name = "labelvaros";
+            this.labelvaros.Size = new System.Drawing.Size(33, 13);
+            this.labelvaros.TabIndex = 14;
+            this.labelvaros.Text = "varos";
+            // 
+            // comboBoxvaros
+            // 
+            this.comboBoxvaros.FormattingEnabled = true;
+            this.comboBoxvaros.Location = new System.Drawing.Point(79, 237);
+            this.comboBoxvaros.Name = "comboBoxvaros";
+            this.comboBoxvaros.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxvaros.TabIndex = 15;
+            // 
+            // latvanyossagarnumeric
+            // 
+            this.latvanyossagarnumeric.Location = new System.Drawing.Point(79, 217);
+            this.latvanyossagarnumeric.Name = "latvanyossagarnumeric";
+            this.latvanyossagarnumeric.Size = new System.Drawing.Size(120, 20);
+            this.latvanyossagarnumeric.TabIndex = 16;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(316, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 17;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listatorlesbutton
+            // 
+            this.listatorlesbutton.Location = new System.Drawing.Point(343, 118);
+            this.listatorlesbutton.Name = "listatorlesbutton";
+            this.listatorlesbutton.Size = new System.Drawing.Size(75, 23);
+            this.listatorlesbutton.TabIndex = 18;
+            this.listatorlesbutton.Text = "listatorles";
+            this.listatorlesbutton.UseVisualStyleBackColor = true;
+            this.listatorlesbutton.Click += new System.EventHandler(this.listatorlesbutton_Click);
+            // 
+            // listamodositbutton
+            // 
+            this.listamodositbutton.Location = new System.Drawing.Point(343, 147);
+            this.listamodositbutton.Name = "listamodositbutton";
+            this.listamodositbutton.Size = new System.Drawing.Size(75, 23);
+            this.listamodositbutton.TabIndex = 19;
+            this.listamodositbutton.Text = "modosit";
+            this.listamodositbutton.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(316, 176);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 326);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listamodositbutton);
+            this.Controls.Add(this.listatorlesbutton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.latvanyossagarnumeric);
+            this.Controls.Add(this.comboBoxvaros);
+            this.Controls.Add(this.labelvaros);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labellatvanyossag);
+            this.Controls.Add(this.textBoxleiras);
+            this.Controls.Add(this.textboxnev);
+            this.Controls.Add(this.buttonlatvanyossag);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonkilepes);
+            this.Controls.Add(this.lakossagnumeric);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonkuldes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lakossagnumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.latvanyossagarnumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonkuldes;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown lakossagnumeric;
+        private System.Windows.Forms.Button buttonkilepes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonlatvanyossag;
+        private System.Windows.Forms.TextBox textboxnev;
+        private System.Windows.Forms.TextBox textBoxleiras;
+        private System.Windows.Forms.Label labellatvanyossag;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelvaros;
+        private System.Windows.Forms.ComboBox comboBoxvaros;
+        private System.Windows.Forms.NumericUpDown latvanyossagarnumeric;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button listatorlesbutton;
+        private System.Windows.Forms.Button listamodositbutton;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Latvanyossagok/Form1.cs
@@ -0,0 +1,134 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latvanyossagok
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection("Server=localhost;Database=latvanyossagokdb;Uid=root;Pwd=;");
+            conn.Open();
+            var command = conn.CreateCommand();
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS `latvanyossagokdb`.`varosok`
+            ( `id` INT NOT NULL AUTO_INCREMENT , `nev` VARCHAR(70) NOT NULL ,
+            PRIMARY KEY (`id`), UNIQUE (`nev`)) ENGINE = InnoDB,
+            ALTER TABLE `varosok` ADD `lakossag` INT NOT NULL AFTER `nev`;
+            ) ";
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS `latvanyossagokdb`.`latvanyossagok` ( `id` INT NOT NULL AUTO_INCREMENT ,
+            `nev` VARCHAR(70) NOT NULL ,
+            `leiras` VARCHAR(70) NOT NULL ,  `ar` INT NOT NULL ,
+            `varos_id` INT NOT NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;)";
+
+        }
+
+        public void varoslistazas()
+        {
+            
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT id, nev, lakossag from varosok";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32("id");
+                    var nev = reader.GetString("nev");
+                    var lakossag = reader.GetInt32("lakossag");
+                    
+                    listBox1.Items.Add(id, nev, lakossag);
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonkuldes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = @"INSERT INTO varosok(nev,lakossag) VALUES (@nev, @lakossag)";
+                    cmd.Parameters.AddWithValue("@nev", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@lakossag", lakossagnumeric.Value);
+                    cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("nem toltott ki mindent");
+                }
+               
+            }
+            catch (MySqlException ex )
+            {
+                if (ex.Number == 1062)
+                {
+                    MessageBox.Show("mar van ilyen nevu varos");
+                }
+                
+            }
+            
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)//latvanyossaggomb
+        {
+            if (!String.IsNullOrWhiteSpace(textboxnev.Text)&& !String.IsNullOrWhiteSpace(textBoxleiras.Text)&& latvanyossagarnumeric.Value <1 && comboBoxvaros.SelectedIndex == -1)
+            {
+                var cmd = conn.CreateCommand();
+            cmd.CommandText = @"INSERT INTO latvanyossagok(nev,leiras,ar,varos_id) VALUES (@nev, @leiras,@ar,@varos_id)";
+            cmd.Parameters.AddWithValue("@nev", textboxnev.Text);
+            cmd.Parameters.AddWithValue("@leiras", textBoxleiras.Text);
+            cmd.Parameters.AddWithValue("@ar", latvanyossagarnumeric.Value);
+            cmd.Parameters.AddWithValue("@varos_id", comboBoxvaros.SelectedItem);
+
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                MessageBox.Show("nem toltott ki mindent");
+            }
+
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listatorlesbutton_Click(object sender, EventArgs e)
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT id, nev, lakossag from varosok";
+            if (varosok)
+            {
+
+            }
+
+        }
+    }
+}
